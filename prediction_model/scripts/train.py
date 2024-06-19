8906143a9afbd7c004156b58de2fe911efa52951
Philip Napoli <dubomb1011@gmail.com>
2024-06-19 17:07:10 -0400

    Trained linear model for testing use

--- prediction_model/scripts/train.py
+++ prediction_model/scripts/train.py
@@ -11,12 +11,15 @@ import testing.testing as testing
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+save_path = 'model_linear.pt'
+
 model = ModelLinear().to(device)
+model.load_state_dict(torch.load(save_path))
 
 train_dir = 'train'
 validate_dir = 'val'
 test_dir = 'test'
-batch_size = 64
+batch_size = 128
 
 training_dataset = dl.get_dataset(train_dir)
 train_loader = dl.get_loader(training_dataset, batch_size)
@@ -30,7 +33,9 @@ test_loader = dl.get_loader(testing_dataset, batch_size)
 loss_fn = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters())
 
-epochs = 5
+epochs = 2
 
 training.train_model(device, train_loader, validation_loader, model, epochs, optimizer, loss_fn)
 testing.test_model(device, test_loader, model, loss_fn)
+
+torch.save(model.state_dict(), save_path)
