a6029d615e3dcb9341499ea8462c67a5bcd54fc7
Dubomb <dubomb1011@gmail.com>
2024-06-20 22:18:34 -0400

    Changed image splitting to divide images based on their center and expand out

--- backend/services/imageservice.py
+++ backend/services/imageservice.py
@@ -3,18 +3,20 @@ import numpy as np
 import base64
 import random
 import os
+import torch
+from torchvision import transforms
 
 blur = (5, 5)
 border_type = 0
 
 lower_threshold = 50
 upper_threshold = 150
-image_scale = 5000
+image_scale = 1000
 
 inter_mode = cv2.INTER_CUBIC
 
 
-def convert_image(encoded):
+def decode_image(encoded):
     image_data = base64.b64decode(encoded)
     np_array = np.frombuffer(image_data, np.uint8)
     image = cv2.imdecode(np_array, cv2.IMREAD_GRAYSCALE)
@@ -37,10 +39,18 @@ def split_image(image):
     bounding_images = []
     for contour in filtered_contours:
         x, y, w, h = cv2.boundingRect(contour)
-        cropped = image[y:y + h, x:x + w]
+        cx = x + w // 2
+        cy = y + h // 2
+        largest_dim = max(abs(cx - x), abs(cy - y))
+        x = cx - largest_dim
+        w = largest_dim * 2
+        y = cy - largest_dim
+        h = largest_dim * 2
+
+        cropped = image[y:y+h, x:x + w]
         bounding_images.append((x, cropped))
-    
-    bounding_images.sort()
+
+    bounding_images.sort(key=lambda image : image[0])
     return bounding_images
 
 
@@ -59,3 +69,9 @@ def add_background(image, bg_width, bg_height, padding):
     bg_ypos = (bg_height - curr_height) // 2
     bg[bg_ypos:bg_ypos+curr_height, bg_xpos:bg_xpos+curr_width] = resized
     return bg
+
+
+def transform_image(image):
+    transformed_image = torch.tensor(image, dtype=torch.float32)
+    input_image = transformed_image.unsqueeze(0)
+    return input_image
