f04997213b5c2f192f2d531df9529b744543ca3c
Dubomb <dubomb1011@gmail.com>
2024-07-23 17:32:42 -0400

    Implemented image capture and backend server call

--- frontend/lib/src/components/screens/homepage.dart
+++ frontend/lib/src/components/screens/homepage.dart
@@ -3,10 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:frontend/src/components/widgets/cameradisplay.dart';
 import 'package:frontend/src/core/services/imageservice.dart' as imageservice;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final CameraDescription camera;
 
   const HomePage({super.key, required this.camera});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(widget.camera, ResolutionPreset.medium);
+    _initializeControllerFuture = _controller.initialize();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,14 +30,19 @@ class HomePage extends StatelessWidget {
         title: const Center(child: Text("MathPics")),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Column(
-        children: [
-          CameraDisplay(camera: camera,),
-          const Text("Hello!"),
-          ElevatedButton(onPressed: () => {
-            imageservice.makePrediction(null).then((res) => {print(res)})
-          }, child: const Text('Take a picture!')),
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            CameraDisplay(controller: _controller, initializeControllerFuture: _initializeControllerFuture,),
+            const Text("Hello!"),
+            ElevatedButton(onPressed: () async {
+              XFile image = await _controller.takePicture();
+              imageservice.makePrediction(image).then((res) {
+                print(res);
+              });
+            }, child: const Text('Take a picture!')),
+          ],
+        ),
       ),
     );
   }
--- frontend/lib/src/components/widgets/cameradisplay.dart
+++ frontend/lib/src/components/widgets/cameradisplay.dart
@@ -2,40 +2,23 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 class CameraDisplay extends StatefulWidget {
-  final CameraDescription camera;
+  final CameraController controller;
+  final Future<void> initializeControllerFuture;
 
-  const CameraDisplay({super.key, required this.camera});
+  const CameraDisplay({super.key, required this.controller, required this.initializeControllerFuture});
 
   @override
   State<CameraDisplay> createState() => _CameraDisplayState();
 }
 
 class _CameraDisplayState extends State<CameraDisplay> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = CameraController(widget.camera, ResolutionPreset.medium);
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    var screenWidth = MediaQuery.of(context).size.width;
-    var screenHeight = MediaQuery.of(context).size.height;
     return FutureBuilder(
-      future: _initializeControllerFuture,
+      future: widget.initializeControllerFuture,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
-          return CameraPreview(_controller);
+          return CameraPreview(widget.controller);
         } else {
           return const Column(
             children: [
